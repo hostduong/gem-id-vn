484cb6476eae4737c5812cba0130f9e93379c6e0
hostduong <hostduong@gmail.com>
2025-06-17 18:12:04 +0700

    Update and rename src/email/add-email.ts to email/add-email.ts

--- email/add-email.ts
+++ email/add-email.ts
@@ -1,7 +1,6 @@
 export async function onRequestPost(context) {
   const { request, env } = context;
-  const body = await request.json();
-  const { username, email, password, type = "outlook", note = "" } = body;
+  const { username, email, password, type = "outlook", note = "" } = await request.json();
 
   const kv = type === "gmail" ? env.KHOAI_KV_GMAIL : env.KHOAI_KV_OUTLOOK;
   const key = `KHOAI/\\/` + (type === "gmail" ? "gmail" : "email") + `/\\/user:${username}:${email}`;
@@ -9,12 +8,13 @@ export async function onRequestPost(context) {
   const exists = await kv.get(key);
   if (exists) return new Response("Email đã tồn tại", { status: 400 });
 
-  const time = new Date().toISOString();
   await kv.put(key, JSON.stringify({
     password, refresh_token: "", access_token: "", client_id: "",
     recovery_phone: "", recovery_email: "", recovery_gmail: "",
-    recovery_password: "", status_token: "Live", note, time_token: 0, time
+    recovery_password: "", status_token: "Live", note,
+    time_token: 0,
+    time: new Date().toISOString()
   }));
 
-  return new Response("Đã thêm email mới", { status: 200 });
+  return new Response("✅ Đã thêm email mới", { status: 200 });
 }
