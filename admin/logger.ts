cc34c7cf5aa00acd78eb7ec88a91e39127909f85
hostduong <hostduong@gmail.com>
2025-06-17 18:23:00 +0700

    Create logger.ts

---
+++ admin/logger.ts
@@ -0,0 +1,10 @@
+// ✅ admin/logger.ts
+export async function onRequestGet(context) {
+  const url = new URL(context.request.url);
+  const username = url.searchParams.get("username");
+  const prefix = `KHOAI/\/log/\/user:${username}`;
+  const list = await context.env.KHOAI_KV_USER.list({ prefix });
+  return new Response(JSON.stringify(list.keys.map(k => k.name)), {
+    headers: { "Content-Type": "application/json" }
+  });
+}
