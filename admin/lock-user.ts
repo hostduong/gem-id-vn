fbfd9bc673cc2b4c1b3ed0d644cdcebd8e110983
hostduong <hostduong@gmail.com>
2025-06-17 18:21:55 +0700

    Create lock-user.ts

---
+++ admin/lock-user.ts
@@ -0,0 +1,10 @@
+// ✅ admin/lock-user.ts
+export async function onRequestPost(context) {
+  const { username = "", lock = true } = await context.request.json();
+  const userKey = `KHOAI/\/profile/\/user:${username}`;
+  const user = await context.env.KHOAI_KV_USER.get(userKey, "json");
+  if (!user) return new Response("Không tìm thấy user", { status: 404 });
+  user.status = lock ? "lock" : "live";
+  await context.env.KHOAI_KV_USER.put(userKey, JSON.stringify(user));
+  return new Response(`✅ Tài khoản đã được ${lock ? "khoá" : "mở"}`, { status: 200 });
+}
