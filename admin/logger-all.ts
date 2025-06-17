f31887dcd1c36f4a13721a815429324e67e50575
hostduong <hostduong@gmail.com>
2025-06-17 18:27:17 +0700

    Update logger-all.ts

--- admin/logger-all.ts
+++ admin/logger-all.ts
@@ -1,5 +1,4 @@
-
-// ✅ admin/logger-all.ts
+// ✅ admin/logger-all.ts - Toàn bộ log hệ thống (theo key prefix)
 export async function onRequestGet(context) {
   const list = await context.env.KHOAI_KV_USER.list({ prefix: "KHOAI/\/log/" });
   return new Response(JSON.stringify(list.keys.map(k => k.name)), {
