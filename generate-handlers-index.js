8716107836b6b87d9683f4b521206ae051688fc0
hostduong <hostduong@gmail.com>
2025-06-19 17:12:29 +0700

    Update generate-handlers-index.js

--- generate-handlers-index.js
+++ generate-handlers-index.js
@@ -12,7 +12,7 @@ let output = `// ✅ Auto-generated handlers/index.ts\n`;
 // ✅ Tự động lấy tất cả thư mục trong src/, bỏ qua api và handlers
 const folders = fs.readdirSync(srcPath).filter(name => {
   const fullPath = path.join(srcPath, name);
-  return fs.statSync(fullPath).isDirectory() && !["api", "handlers"].includes(name);
+  return fs.statSync(fullPath).isDirectory() && !["api", "handlers", "css"].includes(name);
 });
 
 for (const folder of folders) {
