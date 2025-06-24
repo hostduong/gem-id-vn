ecce903b2838b84a23130aa9ef867fa2bc4e5b5b
hostduong <hostduong@gmail.com>
2025-06-24 10:23:36 +0700

    Update worker.ts

--- src/worker.ts
+++ src/worker.ts
@@ -1,5 +1,9 @@
 export default {
   async fetch(request: Request, env: any, ctx: ExecutionContext) {
-    return new Response('<h1>Worker test OK</h1>', { headers: { 'content-type': 'text/html' } });
+    const url = new URL(request.url);
+    if (url.pathname === "/api/register" && request.method === "POST") {
+      // Handle API register...
+    }
+    return fetch(request); // Fallback UI tĩnh
   }
 }
